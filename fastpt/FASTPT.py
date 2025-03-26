0f396eadb3fc075cf120a76f0ef500030b3f5d70
CarterDW <cdw7303@gmail.com>
2025-03-26 14:42:58 -0400

    Renamed and shuffled IAxnl bias terms to fit new standards

--- fastpt/FASTPT.py
+++ fastpt/FASTPT.py
@@ -630,23 +630,14 @@ class FASTPT:
         P_der = (self.k_original**2)*P
         return P_der
     
-    def IA_d2(self,P,P_window=None, C_window=None):
+    def gI_ta(self,P,P_window=None, C_window=None):
         P_fe, A = self.J_k_tensor(P,self.X_IA_gb2_fe, P_window=P_window, C_window=C_window)
         if (self.extrap):
             _, P_fe = self.EK.PK_original(P_fe)
-        P_he, A = self.J_k_tensor(P,self.X_IA_gb2_he, P_window=P_window, C_window=C_window)
-        if (self.extrap):
-            _, P_he = self.EK.PK_original(P_he)
         P_F2, A = self.J_k_tensor(P,self.X_IA_gb2_F2, P_window=P_window, C_window=C_window)
         if (self.extrap):
             _, P_F2 = self.EK.PK_original(P_F2)
         sig4 = np.trapz(self.k_original ** 3 * P ** 2, x=np.log(self.k_original)) / (2. * pi ** 2)
-        P_d2E = P_F2
-        P_d20E = P_he
-        P_d2E2 = P_fe
-        return 2*P_d2E, 2*P_d20E, 2*P_d2E2
-    
-    def IA_s2(self, P, P_window=None, C_window=None):
         P_S2F2, A = self.J_k_tensor(P, self.X_IA_gb2_S2F2, P_window=P_window, C_window=C_window)
         if (self.extrap):
             _, P_S2F2 = self.EK.PK_original(P_S2F2)
@@ -656,13 +647,22 @@ class FASTPT:
         P_S2fe, A = self.J_k_tensor(P, self.X_IA_gb2_S2fe, P_window=P_window, C_window=C_window)
         if (self.extrap):
             _, P_S2fe = self.EK.PK_original(P_S2fe)
+        P_d2E = P_F2
+        P_d20E = P_fe
+        P_s2E=P_S2F2
+        P_s20E=P_S2fe
+        return 2*P_d2E, 2*P_d20E, 2*P_s2E, P_s20E
+    
+    def gI_tt(self, P, P_window=None, C_window=None):
         P_S2he, A = self.J_k_tensor(P, self.X_IA_gb2_S2he, P_window=P_window, C_window=C_window)
         if (self.extrap):
             _, P_S2he = self.EK.PK_original(P_S2he)
-        P_s2E=P_S2F2
-        P_s20E=P_S2fe
+        P_he, A = self.J_k_tensor(P,self.X_IA_gb2_he, P_window=P_window, C_window=C_window)
+        if (self.extrap):
+            _, P_he = self.EK.PK_original(P_he)
+        P_d2E2= P_he
         P_s2E2=P_S2he
-        return 2*P_s2E, 2*P_s20E, 2*P_s2E2
+        return 2*P_d2E2, 2*P_s2E2
 
 
     def OV(self, P, P_window=None, C_window=None):
